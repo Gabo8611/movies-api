6332028fdbbbdc30258af2dce574b2c62dc966cf
Gabo <gabo_chayo_siete@hotmail.com>
2021-03-05 00:00:56 -0600

    cambio de prioridad en sort por año

--- pkg/movies/movies.go
+++ pkg/movies/movies.go
@@ -10,10 +10,10 @@ type Movies []Movie
 
 func (m Movies) Len() int           { return len(m) }
 func (m Movies) Less(i, j int) bool { 
-	if(m[i].Title == m[j].Title){
-		return m[i].Year < m[j].Year
+	if(m[i].Year == m[j].Year){
+		return m[i].Title < m[j].Title
 	}else{
-		return m[i].Title < m[j].Title 
+		return m[i].Year < m[j].Year 
 	} 
 }
 func (m Movies) Swap(i, j int)      { m[i], m[j] = m[j], m[i] }
