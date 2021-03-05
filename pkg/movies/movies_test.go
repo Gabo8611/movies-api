cc33aba50cfd6042375b1897433d14384a488491
Gabo <gabo_chayo_siete@hotmail.com>
2021-03-05 00:13:22 -0600

    ajuste de pruebas movies con prioridad año en sort

--- pkg/movies/movies_test.go
+++ pkg/movies/movies_test.go
@@ -27,6 +27,21 @@ func TestMovies(t *testing.T) {
 			expectedErrorString: "",
 		},
 		{
+			name:             "SortYearCase",
+			mockResponseBody: `{"Search":[{"Title":"Star Wars: Episode IV - A New Hope","Year":"1977"},{"Title":"Star Wars: Episode V - The Empire Strikes Back","Year":"1980"},{"Title":"Star Wars: Episode VI - Return of the Jedi","Year":"1983"},{"Title":"Star Wars: Episode VII - The Force Awakens","Year":"2015"},{"Title":"Star Wars: Episode I - The Phantom Menace","Year":"1999"},{"Title":"Star Wars: Revelations","Year":"2005"},{"Title":"Star Wars: Episode II - Attack of the Clones","Year":"2002"},{"Title":"Star Wars: Episode III - Revenge of the Sith","Year":"2005"}]}`,
+			expectedMovies: []Movie{
+				{Title:"Star Wars: Episode IV - A New Hope",Year:"1977"},
+				{Title:"Star Wars: Episode V - The Empire Strikes Back",Year:"1980"},
+				{Title:"Star Wars: Episode VI - Return of the Jedi",Year:"1983"},
+				{Title:"Star Wars: Episode I - The Phantom Menace",Year:"1999"},
+				{Title:"Star Wars: Episode II - Attack of the Clones",Year:"2002"},
+				{Title:"Star Wars: Episode III - Revenge of the Sith",Year:"2005"},
+				{Title:"Star Wars: Revelations",Year:"2005"},
+				{Title:"Star Wars: Episode VII - The Force Awakens",Year:"2015"},
+			},
+			expectedErrorString: "",
+		},
+		{
 			name:             "SorYearCase",
 			mockResponseBody: `{"Search":[{"Title":"Star Wars: The Empire Strikes Back","Year":"1980"},{"Title":"Star Wars: The Empire Strikes Back","Year":"1977"}]}`,
 			expectedMovies: []Movie{
